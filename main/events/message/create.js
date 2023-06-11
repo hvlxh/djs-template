a1fa9ef5224ffd1b59896829b2bb157ab71aae6f
hvlxh <monsta22019@gmail.com>
2023-06-11 15:41:40 +0530

    Bug fixes

--- main/events/message/create.js
+++ main/events/message/create.js
@@ -16,18 +16,53 @@ class MessageCreate extends Event {
 
     if (!prefixUsed) return
 
-    const raw = message.content.substring(prefixUsed.length)
+    const match = message.content.match(/^\?(\S+)\s*([\s\S]*)$/)
+    if (!match) return
 
-    const cmd = this.client
-      .getPrefixCommands()
-      .find((v, k) => k.startsWith(raw))
-    if (!cmd) return message.reply('Invaild command.')
-    const key = this.client
-      .getPrefixCommands()
-      .findKey((v, k) => k.startsWith(raw))
-    if (!key) return message.reply('Invaild command.')
+    const [, rawCommand, ...args] = match
+    const cmd = this.client.getPrefixCommands().find((v, k) => k === rawCommand)
 
-    const args = message.content.substring(key?.length).split(' ').slice(1)
+    if (!cmd) return message.reply('Invalid command.')
+    cmd.options.permissions?.member?.forEach((perm) => {
+      if (!message.member?.permissionsIn(message.channelId).has(perm))
+        return message.reply(
+          "You didn't have permission to use this command. (`{}`)".replace(
+            '{}',
+            perm.toString()
+          )
+        )
+    })
+
+    cmd.options.permissions?.bot?.forEach(async (perm) => {
+      if (
+        !(await message.guild?.members.fetchMe())
+          ?.permissionsIn(message.channelId)
+          .has(perm)
+      )
+        return message.reply(
+          "Bot didn't have permission to execute this command. (`{}`)".replace(
+            '{}',
+            perm.toString()
+          )
+        )
+    })
+
+    if (cmd.options.arguments.min <= args.length) {
+      return message.reply(
+        'Minimum arguments is {1} but received {2}'
+          .replace('{1}', cmd.options.arguments.min.toString())
+          .replace('{2}', args.length.toString())
+      )
+    }
+
+    if (cmd.options.arguments.max >= args.length) {
+      return message.reply(
+        'Maximum arguments is {1} but received {2}'
+          .replace('{1}', cmd.options.arguments.min.toString())
+          .replace('{2}', args.length.toString())
+      )
+    }
+
     cmd.run({ message, client: this.client, options: args })
   }
 }
